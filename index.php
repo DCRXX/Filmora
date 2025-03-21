788d96f182f3afeb03c14ae39543115867e4ec7e
Алексей <linnik654g@gmail.com>
2025-03-21 17:57:41 +0300

    FTFT

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Filmora</title>
-    <link rel="stylesheet" href="./style.css?v=1">
+    <link rel="stylesheet" href="style.css?=12">
     <link rel="icon" type="image/svg+xml" href="image/F.svg">
     <link rel="stylesheet" href="./swiper-bundle.min.css">
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
@@ -35,8 +35,11 @@
         <div class="Mess">
             <img src="image/notifications.svg">
         </div>
-        <div class="Accaunt">
-            <p>Войти</p>
+        <a href="login/login.php">
+            <div class="Accaunt">
+                <p>Войти</p>
+            </div>
+        </a>
         </div>
     </section>
     <section class="Top-Films">
@@ -50,25 +53,7 @@
                 }
 
                 $sql = "SELECT title, poster_url, genre, duration, rating, release_year 
-                    FROM `0000`
-                    UNION
-                    SELECT title, poster_url, genre, duration, rating, release_year 
-                    FROM `0001`
-                    UNION
-                    SELECT title, poster_url, genre, duration, rating, release_year 
-                    FROM `0002`
-                    UNION
-                    SELECT title, poster_url, genre, duration, rating, release_year 
-                    FROM `0003`
-                    UNION
-                    SELECT title, poster_url, genre, duration, rating, release_year 
-                    FROM `0004`
-                    UNION
-                    SELECT title, poster_url, genre, duration, rating, release_year 
-                    FROM `0005`
-                    UNION
-                    SELECT title, poster_url, genre, duration, rating, release_year 
-                    FROM `0006`
+                    FROM `movies` WHERE id IN (1,2,3,13,14,21)
                     LIMIT 7";
             $result = $conn->query($sql);
 
@@ -100,42 +85,140 @@
     </section>
     <section class="pro">
         <div class="Podpiska">
-            <div class="Name_pro">
-                <p>Подписка</p>
+            <div class="hh1">
+                <div class="podpiska">
+                    <p>Подписка</p>
+                    <div class="pro_name">
+                        <p>Pro</p>
+                    </div>
+                </div>
                 <div class="name">
-                    <h1>Pro</h1>
+                    <p>Ваш кинотеатр дома</p>
+                </div>
+                <div class="opis">
+                    <p>новинки, эксклюзивы, любимые хиты - всё в одной подписке</p>
+                </div>
+                <div class="cena">
+                    <div class="pp">
+                        <p>1 месяц бесплатно</p>
+                    </div>
+                    <div class="pp1">
+                        <p>далее- 199руб</p>
+                    </div>
+                </div>
+                <div class="ohorm">
+                    <div class="ohor">
+                        <p>оформить подписку</p>
+                    </div>
+                    <div class="podrob">
+                        <p>Подробнее</p>
+                    </div>
                 </div>
             </div>
-            <div class="Prev">
-                <p>Ваш кинотеатр дома</p>
-            </div>
+            <div class="hh2">
+                <div class="Phot">
+                    <img class="One" src="./image/ybic.svg" width="150px" height="200px">
+                    <img class="Two" src="./image/fallout.svg"  width="130px" height="175px">
+                    <img class="Trhee" src="./image/600x900.svg"  width="110px" height="150px">
+                    <img class="Four" src="./image/9.svg"  width="82px" height="120px">
+                </div>
+                <div class="FTR">
+                    <p>более 100</p>
+                </div>
+                <div class="FTT">
+                    <p>Фильмов, Сериалов</p>
+                </div>
+                <div class="FTT1">
+                    <p>Мультфильмов, аниме</p> 
+                </div>
+            </div> 
+        </div>   
+    </section>
+    <section class="reki">
+        <div class="recom">
+            <p>Рекомендации</p>
+            <img src="./image/Component 1.svg">
+        </div>
+        <div class="FRR">
+            <?php
+                $conn = new mysqli("localhost", "root", "root", "movies_db");
+
+                if ($conn->connect_error) {
+                    die("Ошибка подключения: " . $conn->connect_error);
+                }
+
+                $sql = "SELECT poster_url, duration, rating, subscription_type, title
+                    FROM `movies` WHERE id IN (4,5,6,16,17,19,20,18,22,23)
+                    LIMIT 10";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo '<div class="F">';
+                    echo '<div class="Film-recom">';
+                    echo '<img src="' . htmlspecialchars($row["poster_url"]) . '" alt="' . htmlspecialchars($row["title"]) . '" style="width: 200px; height: 300px; border-radius: 15px;">';
+                        echo '<div class="film-info-F">';
+                        echo '<div class="black-fon">';
+                            echo '<div class="subscription_type">';
+                                echo '<p>' . htmlspecialchars($row["subscription_type"]) .'</p>';
+                            echo '</div>';
+                            echo '<div class="Rating-F-recom">';
+                                echo '<p>' . htmlspecialchars($row["rating"]) .'</p>';
+                            echo '</div>';
+                            echo '<div class="ostal-F">';
+                                echo '<p>' . htmlspecialchars($row["duration"]) . '</p>';
+                            echo '</div>';
+                        echo '</div>';
+                    echo '</div>';
+                    echo '</div>';
+                }
+                } else {
+                    echo '<div class="Film-recom">Нет данных</div>';
+                }
+
+                $conn->close();
+                ?>
         </div>
     </section>
+    <section class="Top-10">
+        <div class="N-Top">
+            <p>Топ-10 за месяц</p>
+            <img src="./image/Component 1.svg">
+        </div>
+        <div class="Top-F">
+            <?php
+                $conn = new mysqli("localhost", "root", "root", "movies_db");
+
+                if ($conn->connect_error) {
+                    die("Ошибка подключения: " . $conn->connect_error);
+                }
+
+                $sql = "SELECT poster_url, Number
+                    FROM `movies` WHERE id IN (4,5,6,16,17,18,19,20,28,30)
+                    LIMIT 10";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo '<div class="Number">';
+                        echo '<p>' . htmlspecialchars($row["Number"]) .'</p>';
+                    echo '</div>';
+                    echo '<div class="T-F">';
+                        echo '<img src="' . htmlspecialchars($row["poster_url"]) . '" alt="' . htmlspecialchars($row["title"]) . '" style="width: 255px; height: 385px; border-radius: 15px;">';
+                    echo '</div>';
+                }
+                } else {
+                    echo '<div class="Film-recom">Нет данных</div>';
+                }
+
+                $conn->close();
+                ?>
+        </div>
+    </section>
+    
+    
+
     
     <script src="script.js"></script>
 </body>
 </html>
-
-<?php
-
-$conn = new mysqli("localhost", "root", "root", "movies_db");
-
-if ($conn->connect_error) {
-    die("Ошибка подключения: " . $conn->connect_error);
-}
-
-$sql = "SELECT title FROM movies LIMIT 7";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo '<div class="swiper-slide">' . htmlspecialchars($row["title"]) . '</div>';
-    }
-} else {
-    echo '<div class="swiper-slide">Ошибка: Нет данных или запрос не выполнен</div>';
-}
-
-$conn->close();
-?>
-
-ветренный
